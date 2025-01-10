69a9636ab4c852f2e36ca8c88562720bb345794e
sharvari1614 <sharvaribodas@gmail.com>
2025-01-11 02:08:03 +0530

    version

--- src/main/java/com/example/todolist3/Todolist3Application.java
+++ src/main/java/com/example/todolist3/Todolist3Application.java
@@ -3,18 +3,18 @@ package com.example.todolist3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class Todolist3Application {
-    
+
     @PostMapping("/display")
-    public String hello(){
+    public String hello() {
         return "Hello world";
     }
 
-	public static void main(String[] args) {
-		SpringApplication.run(Todolist3Application.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(Todolist3Application.class, args);
+    }
 }
- 
